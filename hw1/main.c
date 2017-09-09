85f72bd5a2a3c7aed5dadf9188bbf56c5efcaae3
Raj_Kumar <raj.subramaniam@colorado.edu>
2017-09-08 22:09:21 -0700

    Final Submission

--- hw1/main.c
+++ hw1/main.c
@@ -1,3 +1,9 @@
+/**
+ * @file main.c 
+ * @brief Code to test the double linked list and circular buffer for HW1
+ * @author Raj Kumar Subramaniam
+ * @date September 5,2017
+**/
 #include <stdio.h>
 #include <stdint.h>
 #include "doublyLinked.h"
@@ -8,14 +14,15 @@ int main()
 	DL_t* parser = NULL;
 	size_t search;
 	uint32_t data;
+	/*Adding Nodes*/
 	printf("main::Add node:%d\n",DL_AddNode(&head,1,0));
 	printf("main::Add node:%d\n",DL_AddNode(&head,2,1));
 	printf("main::Add node:%d\n",DL_AddNode(&head,3,2));
 	printf("main::Add node:%d\n",DL_AddNode(&head,4,3));
 	printf("main::Add node:%d\n",DL_AddNode(&head,5,0));
-	DL_Search(head,4,&search);
+	DL_Search(head,4,&search);		/*Search a node*/
 	printf("Found at %ld\n",search);	
-	DL_RemoveNode(&head,&data,0);
+	DL_RemoveNode(&head,&data,0);	/*Remove Node*/
 	DL_RemoveNode(&head,&data,3);
 	DL_RemoveNode(&head,&data,1);
 	parser = head;
@@ -25,8 +32,9 @@ int main()
 		parser = parser->next;
 	}
 	printf("\nCircular Buffer\n");
-	DL_Destroy(&head);
+	DL_Destroy(&head);		/*Free all the nodes*/
 	
+	/*Circular Buffer Test*/
 	CB_t *buffer = NULL;
 	CirBuffInit(&buffer,10);
 	for(int i=0;i<5;i++)
